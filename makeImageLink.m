d5627d718e30189f2ed6ba7fcbd521a8302eab7c
Rohit Girdhar <rohit.iiith@gmail.com>
2014-09-14 16:29:11 -0400

    support for ticks etc

--- makeImageLink.m
+++ makeImageLink.m
@@ -1,14 +1,14 @@
 %Author: Ishan Misra (ishan@cmu.edu)
 %Date: Dec, 2013
 
-function linkStr = makeImageLink(imgPath,width,height,altText,borderString)
+function linkStr = makeImageLink(imgPath,width,altText,borderString)
 if(~exist('altText','var'))
-    linkStr=sprintf('<a href="%s"><img src="%s" width=%d height=%d /></a>',imgPath,imgPath,width,height);
+    linkStr=sprintf('<a href="%s"><img src="%s" width=%d /></a>',imgPath,imgPath,width);
 else
     if(~exist('borderString','var'))
-        linkStr=sprintf('<a href="%s"><img src="%s" width=%d height=%d Title="%s"/></a>',imgPath,imgPath,width,height,altText);
+        linkStr=sprintf('<a href="%s"><img src="%s" width=%d Title="%s"/></a>',imgPath,imgPath,width,altText);
     else
-        linkStr=sprintf('<a href="%s"><img src="%s" width=%d height=%d Title="%s" style="%s"/></a>',...
-            imgPath,imgPath,width,height,altText,borderString);
+        linkStr=sprintf('<a href="%s"><img src="%s" width=%d Title="%s" style="%s"/></a>',...
+            imgPath,imgPath,width,altText,borderString);
     end
 end
--- makeImageLinkHover.m
+++ makeImageLinkHover.m
@@ -1,11 +1,22 @@
 %Author: Ishan Misra (ishan@cmu.edu)
 %Date: Dec, 2013
 
-function linkStr = makeImageLink(imgPath1,imgPath2,width,height,params)
-if(~exist('params','var'))	
-linkStr=sprintf('<a href="%s"><img src="%s" width=%d height=%d onmouseover="this.src=''%s''" onmouseout="this.src=''%s''" /></a>',imgPath1,imgPath1,width,height,imgPath2,imgPath1);
+function linkStr = makeImageLinkHover(imgPath1,imgPath2,width,params)
+if(~exist('params','var'))
+    linkStr=sprintf('<div><a href="%s"><img src="%s" width=%d onmouseover="this.src=''%s''" onmouseout="this.src=''%s''" /></a>',imgPath1,imgPath1,width,imgPath2,imgPath1);
 else
-if(isfield(params,'bbox'))
-linkStr=sprintf('<a href="%s"><img src="%s" width=%d height=%d /></a>',imgPath,imgPath,width,height);
-end
+    if isfield(params, 'hover_width')
+        hover_width = params.hover_width;
+    else
+        hover_width = width;
+    end
+    if(isfield(params,'bbox') && params.bbox)
+        linkStr=sprintf('<a href="%s"><img src="%s" width=%d /></a>',imgPath,imgPath,width);
+    else
+        linkStr=sprintf('<div style="position: relative; left: 0; top: 0;"><a href="%s"><img src="%s" width=%d onmouseover="this.src=''%s''; this.width=''%d''" onmouseout="this.src=''%s''; this.width=''%d''" style="position: relative; top: 0; left: 0;" /></a>',imgPath1,imgPath1,width,imgPath2,hover_width,imgPath1,width);
+        if (isfield(params, 'check') && params.check)
+            linkStr = [linkStr, sprintf('<img src="static/check.png" style="position: absolute; bottom: -5px; right: -5px; width: %d;" />', width / 5)];
+        end
+        linkStr = [linkStr, '</div>'];
+    end
 end
